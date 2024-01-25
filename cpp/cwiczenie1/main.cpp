62b35c35c0bc2b86f27858a08bc82054ab425850
Piotero1 <pstanclik_20i3@e.zseeim.edu.pl>
2024-01-25 09:19:50 +0000

    studentmachine send

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 
 int main(){
-	cout<<"Welkomen Sie, Wir mussen Judgen aufwrotten."<<endl;
+
+float trasa;
+float spalanieA;
+float spalanie;
+float cena_litr;
+float koszt;
+float osoby;
+float cena_osoby;
+cout<<"podaj trase"<<endl;
+cin>>trasa;
+cout<<"podaj średnie spalanie: "<<endl;
+cin>>spalanie;
+cout<<"podaj cene za litr paliwa: "<<endl;
+cin>>cena_litr;
+cout<<"podaj ile osob jedzie w samochodzie: "<<endl;
+cin>>osoby;
+
+spalanieA = trasa*spalanie/100;
+koszt = spalanieA*cena_litr;
+cena_osoby = koszt/osoby;
+
+cout<<"koszt za osobe to"<<cena_osoby<<endl;
+
+
+
 	return 0;
 }
